f6c19b520e5ee66c99f4f1ac3f4bfc92d492ad13
HugoVirot <hugo.virot@gmail.com>
2021-03-02 14:17:01 +0100

    version de départ

--- admin/index.php
+++ admin/index.php
@@ -4,25 +4,25 @@ require '../config.php';
 
 session_start();
 
-// If request a logout....
+// demande de logout
 
-if(isset($_GET['logout'])){
+if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION);
 }
 
-// If request un login
+// tentative de login
 
-if(isset($_POST['username']) && isset($_POST['password'])){
+if (isset($_POST['username']) && isset($_POST['password'])) {
 
-	$username = $_POST['username'];
-	$hash = md5($_POST['password']);
+	$login = $_POST['username'];
+	$hashedPassword = md5($_POST['password']);
 
-	$sql = "select * from users where login = '$username' and password = '$hash'";
+	$sql = "select * from users where login = '$login' and password = '$hashedPassword'";
 	$query = $database->query($sql, PDO::FETCH_ASSOC);
 	$users = $query->fetchAll();
-	
-	if(isset($users[0])){
+
+	if (isset($users[0])) {
 		$_SESSION['user'] = $users[0];
 	} else {
 		$loginError = true;
@@ -31,135 +31,142 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 
 ?>
 <html>
-	<head>
-		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,200&display=swap" rel="stylesheet">
-		<style>
-			/* Reset du pauvre... */
-			*{
-				margin:0;
-				padding:0;
-			}
 
-			body, html{
-				height: 100%;
-			}
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,200&display=swap" rel="stylesheet">
+	<style>
+		
+		* {
+			margin: 0;
+			padding: 0;
+		}
 
-			.logo{
-				height: 120px;
-				display: block;
-				margin: 0 auto;
-			}
+		body,
+		html {
+			height: 100%;
+		}
 
-			body{
-				font-family: 'Source Sans Pro', sans-serif;
-				background: #edeff1;
-			}
+		.logo {
+			height: 120px;
+			display: block;
+			margin: 0 auto;
+		}
 
-			.background{
-				position: fixed;
-				perspective: 200px;
-				top: 100%;
-				transform: translateY(-59%) scale(1.5);
-				width: 100%;
-				text-align: center;
-				opacity: .2;
-				pointer-events: none;
-				perspective-origin: center;
-    			z-index: -1;
+		body {
+			font-family: 'Source Sans Pro', sans-serif;
+			background: #edeff1;
+		}
 
-			}
+		.background {
+			position: fixed;
+			perspective: 200px;
+			top: 100%;
+			transform: translateY(-59%) scale(1.5);
+			width: 100%;
+			text-align: center;
+			opacity: .2;
+			pointer-events: none;
+			perspective-origin: center;
+			z-index: -1;
 
-			.background img{
-				width: 100%;
-				transform: rotateX(32deg);
-			}
+		}
 
-			.container{
-				margin:0 auto;
-				background: white;
-				border-radius: 4px;
-				box-shadow: 2px 2px 7px 0px #00000029;
-				padding:5px;
-				width: -moz-fit-content;
-                width: fit-content;
-				margin-bottom:50px;
-			}
+		.background img {
+			width: 100%;
+			transform: rotateX(32deg);
+		}
 
-			a{
-			    color: inherit;
-			}
+		.container {
+			margin: 0 auto;
+			background: white;
+			border-radius: 4px;
+			box-shadow: 2px 2px 7px 0px #00000029;
+			padding: 5px;
+			width: -moz-fit-content;
+			width: fit-content;
+			margin-bottom: 50px;
+		}
 
-			table{
-				border-collapse: collapse;
-				width: 100%;
-			}
+		a {
+			color: inherit;
+		}
 
-			table td, table th{
-				padding:5px 8px;
-			}
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
 
-			table tbody tr:nth-child(odd){
-				background: #f00;
-				background: #dcdcdc;
-			}
+		table td,
+		table th {
+			padding: 5px 8px;
+		}
 
-			label, input, button{
-				display: block;
-			}
+		table tbody tr:nth-child(odd) {
+			background: #f00;
+			background: #dcdcdc;
+		}
 
-			input, button {
-				font-size: 17px;
-				line-height: 30px;
-				padding: 7px 15px;
-				border: 1px solid;
-				border-radius: 4px;
-				margin: 1em;
-				width: 350px;
-				background: transparent;
-				outline: none;
-			}
+		label,
+		input,
+		button {
+			display: block;
+		}
 
-			button{
-				background: #000;
-				color: #fff;
-				border-radius: 4px;
-			}
+		input,
+		button {
+			font-size: 17px;
+			line-height: 30px;
+			padding: 7px 15px;
+			border: 1px solid;
+			border-radius: 4px;
+			margin: 1em;
+			width: 350px;
+			background: transparent;
+			outline: none;
+		}
 
-            hr{
-                border: 1px solid #d4d4d4;
-                border-bottom: none;
-                margin: 15px 15px;
-            }
+		button {
+			background: #000;
+			color: #fff;
+			border-radius: 4px;
+		}
 
-			p{
-				margin: 7px 15px;
-			}
+		hr {
+			border: 1px solid #d4d4d4;
+			border-bottom: none;
+			margin: 15px 15px;
+		}
 
-			.error{
-				padding: 7px 15px;
-				background: #ffa8a8;
-				border: 1px solid;
-				color: #904747;
-				border-radius: 4px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="background"><img src="/img/background.svg"></div>
+		p {
+			margin: 7px 15px;
+		}
 
-		<a href="/"><img src="/img/logo.png" class="logo"></a>
+		.error {
+			padding: 7px 15px;
+			background: #ffa8a8;
+			border: 1px solid;
+			color: #904747;
+			border-radius: 4px;
+		}
+	</style>
+</head>
 
-		<div class="container">
-			<?php if(isset($_SESSION['user'])): ?>
+<body>
+	<div class="background"><img src="/img/background.svg"></div>
 
-                <p>
-                    Bonjour <?=$_SESSION['user']['login']?>
-                    <a style="float:right;" href="?logout">Logout</a>
-                </p>
-                <hr>
-                <?php if(!$_SESSION['user']['admin']):?>
-                <p class="error">Vous devez être administrateur pour afficher les liens.</p>
-                <?php else: ?>
+	<a href="/"><img src="/img/logo.png" class="logo"></a>
+
+	<div class="container">
+		<?php if (isset($_SESSION['user'])) : ?>
+
+			<p>
+				Bonjour <?= $_SESSION['user']['login'] ?>
+				<a style="float:right;" href="?logout">Logout</a>
+			</p>
+			<hr>
+			<?php if (!$_SESSION['user']['admin']) : ?>
+				<p class="error">Vous devez être administrateur pour afficher les liens.</p>
+			<?php else : ?>
 				<table>
 					<thead>
 						<td>Target</td>
@@ -168,30 +175,31 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 					<tbody>
 						<?php
 						$query = $database->query('select * from urls');
-						foreach($query->fetchAll() as $url): ?>
-						<tr>
-							<td>
-								<a href="<?=$url['target']?>" target="_blank">
-									<?=$url['target']?>
-								</a>
-							</td>
-							<td><?=$url['code']?></td>
-						</tr>
+						foreach ($query->fetchAll() as $url) : ?>
+							<tr>
+								<td>
+									<a href="<?= $url['target'] ?>" target="_blank">
+										<?= $url['target'] ?>
+									</a>
+								</td>
+								<td><?= $url['code'] ?></td>
+							</tr>
 						<?php endforeach ?>
 					</tbody>
 				</table>
-                <?php endif; ?>
-            <?php else: ?>
-				<?php if(isset($loginError)): ?>
-					<p class="error">Login error, please try again.</p>
-				<?php endif; ?>
-				<form action="" method="post">
-					<input type="text" name="username" placeholder="Username">
-					<input type="password" name="password" placeholder="Password">
-
-					<button>Login</button>
-				</form>
 			<?php endif; ?>
-		</div>
-	</body>
+		<?php else : ?>
+			<?php if (isset($loginError)) : ?>
+				<p class="error">Login error, please try again.</p>
+			<?php endif; ?>
+			<form action="" method="post">
+				<input type="text" name="username" placeholder="Username">
+				<input type="password" name="password" placeholder="Password">
+
+				<button>Login</button>
+			</form>
+		<?php endif; ?>
+	</div>
+</body>
+
 </html>
--- page/api.php
+++ page/api.php
@@ -3,7 +3,6 @@
 // Insert 
 
 $baseUrl = 'http://'.$_SERVER['HTTP_HOST'].'/?q=';
-$target = $_POST['target'];
 $code = uniqid();
 
 $query = $database->prepare('INSERT INTO urls (code, target) VALUES (:code, :target)');
@@ -13,7 +12,8 @@ $query->execute([
 ]);
 
 
-// Return content
+// On retourne une url constituée de l'adresse de base + le code généré
+// en cliquant sur celle-ci, on peut accéder au site
 
 header('Content-Type: application/json');
 die(json_encode([
